0858ca0c861a3eafdb84b9871cead1b9842fb658
Alex <yxfacw@163.com>
2018-10-15 18:24:02 +0800

    add main activity

---
+++ app/src/main/java/com/alex/zt/support/ActivityUtil.kt
@@ -0,0 +1,11 @@
+package com.alex.zt.support
+
+import android.content.Intent
+import android.support.v4.content.ContextCompat.startActivity
+import android.view.View
+import com.alex.zt.MainActivity
+
+class ActivityUtil {
+
+
+}
